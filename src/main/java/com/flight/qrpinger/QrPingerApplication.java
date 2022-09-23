b88362e084dc0f5b59ca45852bfb405d0ee736c3
SMTanami <58055325+SMTanami@users.noreply.github.com>
2022-09-23 12:55:55 -0700

    initial commit

---
+++ src/main/java/com/flight/qrpinger/QrPingerApplication.java
@@ -0,0 +1,13 @@
+package com.flight.qrpinger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrPingerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrPingerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/flight/qrpinger/QrPingerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flight.qrpinger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrPingerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
