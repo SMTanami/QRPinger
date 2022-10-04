f7e189a62298b4750fe98d56afb95b3b3ed3ac6b
SMTanami <58055325+SMTanami@users.noreply.github.com>
2022-10-03 22:04:04 -0700

    UserController now more SRP compliant and added POST mapping

--- src/main/java/com/flight/qrpinger/controller/UserController.java
+++ src/main/java/com/flight/qrpinger/controller/UserController.java
@@ -7,8 +7,6 @@ import com.flight.qrpinger.service.qrgen.QRService;
 import com.flight.qrpinger.service.sms.TextService;
 import com.flight.qrpinger.service.user.UserService;
 import com.google.zxing.WriterException;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -36,21 +34,38 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity newUser(@RequestBody User user) {
-        User savedUser = userService.saveUser(user);
-        try {
-            QRCode qrCode = qrService.generate(user);
-            emailService.sendEmail(user, "QRPinger - Your QR Code", "Enjoy!", qrCode);
-        } catch (WriterException | IOException | MessagingException e) {
-            //TODO - Handle these errors
-        }
-        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
+        return saveAndRespond(user);
+    }
+
+    @PostMapping("/signup/")
+    public ResponseEntity newUserParams(@RequestParam(value = "firstName") String firstName,
+                                        @RequestParam(value = "lastName") String lastName,
+                                        @RequestParam(value = "phone") String phoneNumber,
+                                        @RequestParam(value = "email") String email) {
+        return saveAndRespond(User.builder()
+                .firstName(firstName)
+                .lastName(lastName)
+                .phoneNumber(phoneNumber)
+                .email(email).build());
     }
 
     @GetMapping("/{id}")
     ResponseEntity pingUser(@PathVariable Long id) {
         User user = userService.getUser(id);
         textService.sendText(user);
+
         return ResponseEntity.ok(user);
+    }
+
+    private ResponseEntity saveAndRespond(User user) {
+        User savedUser = userService.saveUser(user);
+        try {
+            QRCode qrCode = qrService.generate(user);
+            emailService.sendEmail(user,"QRPinger - Your QR Code", "Enjoy!", qrCode);
+        } catch (WriterException | IOException | MessagingException e) {
+            //TODO - Handle these errors
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).body(savedUser);
     }
 
 }
