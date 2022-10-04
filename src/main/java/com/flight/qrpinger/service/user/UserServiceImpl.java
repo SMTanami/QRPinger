e694f19b18d13ce881be6f2b4d7fa818edbe9c0f
David Harrison <david@davidbharrison.com>
2022-10-03 23:33:14 -0700

    replace logger

--- src/main/java/com/flight/qrpinger/service/user/UserServiceImpl.java
+++ src/main/java/com/flight/qrpinger/service/user/UserServiceImpl.java
@@ -4,6 +4,7 @@ import com.flight.qrpinger.domain.User;
 import com.flight.qrpinger.exceptions.AlreadyAUserException;
 import com.flight.qrpinger.exceptions.UserNotFoundException;
 import com.flight.qrpinger.repository.UserRepository;
+import lombok.extern.java.Log;
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -11,22 +12,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
+@Log
 @Service
 public class UserServiceImpl implements UserService {
 
-    private final Logger logger;
     private final UserRepository userRepository;
 
     public UserServiceImpl(UserRepository userRepository) {
         this.userRepository = userRepository;
-        logger = LogManager.getLogger(UserServiceImpl.class);
     }
 
     @Override
     public User saveUser(User user) {
         User containedUser = userRepository.findByEmail(user.getEmail());
         if (containedUser != null) {
-            logger.log(Level.ERROR, "User [" + user + "] already exists. Throwing new AlreadyAUserException...");
+            log.severe("User [" + user + "] already exists. Throwing new AlreadyAUserException...");
             throw new AlreadyAUserException("User with that email already exists");
         }
         return userRepository.save(user);
@@ -36,7 +36,7 @@ public class UserServiceImpl implements UserService {
     public User getUser(Long id) {
         Optional<User> userOptional = userRepository.findById(id);
         if (!userOptional.isPresent()) {
-            logger.log(Level.ERROR, "No user with id [" + id + "] Throwing UserNotFoundException...");
+            log.severe("No user with id [" + id + "] Throwing UserNotFoundException...");
             throw new UserNotFoundException("No user with that id exists...");
         }
 
